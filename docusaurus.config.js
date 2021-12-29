ca93f1b6185c4c85a1f53d73aca3ce0f6886e27e
linux_china <libing.chen@gmail.com>
2021-12-29 14:37:48 -0800

    Polishing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'DBang',
-    tagline: 'Deno CLI scripts management',
+    title: 'dbang',
+    tagline: 'CLI to manage Deno scripts on GitHub',
     url: 'https://dbang.dev',
     baseUrl: '/',
     onBrokenLinks: 'throw',
